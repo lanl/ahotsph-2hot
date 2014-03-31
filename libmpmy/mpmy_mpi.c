82bef871869a3ee37398a962b4f0b8567adbf960
Michael Warren <msw@titan.ccs.ornl.gov>
2014-03-31 00:35:44 -0400

    Generate backtrace on abort using libunwind

--- libmpmy/mpmy_mpi.c
+++ libmpmy/mpmy_mpi.c
@@ -406,6 +406,28 @@ MPMY_JobRemaining(void)
 }
 #endif
 
+#define HAVE_MPMY_DIAGNOSTIC
+
+#define UNW_LOCAL_ONLY
+#include <libunwind.h>
+
+void MPMY_Diagnostic(int (*printflike)(const char *, ...)) {
+    unw_cursor_t cursor;
+    unw_context_t uc;
+
+    unw_getcontext(&uc);
+    unw_init_local(&cursor, &uc);
+
+    while (unw_step(&cursor) > 0) {
+        unw_word_t offset, pc;
+        char fname[64] = {[0] = '\0'};
+	
+        unw_get_reg(&cursor, UNW_REG_IP, &pc);
+        unw_get_proc_name(&cursor, fname, sizeof(fname), &offset);
+        printflike ("%p : (%s+0x%x) [%p]\n", pc, fname, offset, pc);
+    }
+}
+
 #ifdef USE_HWCLOCK
 #include "timers_hwclock.c"
 #endif
