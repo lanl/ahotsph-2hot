795340634da1edd04c8f37d980f269042d5e0d0b
Michael Warren <msw@titan.ccs.ornl.gov>
2014-04-20 18:58:42 -0400

    Working for 1 processor

--- apps/sindex/sindex.c
+++ apps/sindex/sindex.c
@@ -17,20 +17,22 @@
 int
 main(int argc, char *argv[])
 {
+    int level = 3;
+
     MPMY_Init(&argc, &argv);
 
-    if (argc != 3) {
-	singlPrintf("usage: %s infile index\n", argv[0]);
+    if (argc != 2 && argc != 3) {
+	singlPrintf("usage: %s infile [level]\n", argv[0]);
 	exit(1);
     }
     char *infile = argv[1];
+    if (argc == 3) level = atoi(argv[2]);
 
     SDF *sdf = SDFopen(NULL, infile);
     if (!sdf) Error("SDFopen %s failed\n", infile);
 
     int64_t gnobj;
     if (SDFgetint64(sdf, "npart", &gnobj)) Error("SDFget npart failed\n");
-    singlPrintf("gnobj is %ld\n", gnobj);
 
     float a, R[NDIM], rmin[NDIM], rmax[NDIM];
     SDFgetfloatOrDie(sdf, "Rx",  &R[0]);
@@ -40,13 +42,16 @@ main(int argc, char *argv[])
     
     VV(rmin, = -a*R);
     VV(rmax, = a*R);
+
+    /* expand root for non-power-of-two */
+    VS(rmin, *= (1.0 + 0.6)); 
+    VS(rmax, *= (1.0 + 0.6));
+
     FixRsizeExact(rmin, rmax);
 
     int64_t offset = SDFfileoffset("x", sdf);
     int64_t stride = SDFfilestride("x", sdf);
     int64_t len = SDFnrecs("x", sdf);
-
-    singlPrintf("offset is %ld nrecs is %ld stride is %ld\n", offset, len, stride);
 
     assert(stride == sizeof(body));
     assert(len == gnobj);
@@ -61,10 +66,30 @@ main(int argc, char *argv[])
 
     body *btab = mm + offset;
 
-    int64_t i = atoll(argv[2]);
-    Key_t key = GetKeyFast(&btab[i]);
-    singlPrintf("%12.8g %12.8g %12.8g %s\n", 
-		btab[i].pos[0], btab[i].pos[1], btab[i].pos[2], PrintKey(key));
+    singlPrintf("# index base len octal_key\n");
+    singlPrintf("# level=%d\n", level);
+
+    int64_t mask = (1LL<<(level*NDIM))-1;
+    for (int64_t i = 0; i < gnobj; /* NULL */) {
+	int64_t i0 = i;
+	Key_t this_cell = KeyRshift(GetKeyFast(&btab[i0]), NDIM*(BITS_PER_DIM-level));
+	while (1) {
+	    while (KeyEQ(this_cell, KeyRshift(GetKeyFast(&btab[i]), NDIM*(BITS_PER_DIM-level)))) i++;
+	    /* keys are sorted in file by positions on previous timestep.  Sigh. */
+	    /* If at least 8 of next 10 are not in this cell, then it's really a new cell */
+	    int count = 0;
+	    Key_t next_cell = KeyRshift(GetKeyFast(&btab[i]), NDIM*(BITS_PER_DIM-level));
+	    for (int64_t j = i+1; j < i+11; j++) {
+		if (KeyEQ(next_cell, KeyRshift(GetKeyFast(&btab[j]), NDIM*(BITS_PER_DIM-level)))) count++;
+	    }
+	    if (count >= 8) {
+		singlPrintf("%8ld %12ld %12ld %s\n", this_cell.k[0] & mask, i0, i-i0, PrintKey(GetKeyFast(&btab[i0])));
+		break;
+	    } else {
+		i++;
+	    }
+	}
+    }
 
     SDFclose(sdf);
     MPMY_Finalize();
